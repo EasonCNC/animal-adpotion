98d22be38f609bd455914ef08d221990b7d11a07
EASONCNC <eason19984949@gmail.com>
2025-05-25 14:14:57 +0800

    delete material and update urls.py

--- adoption/settings.py
+++ adoption/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'material',
     'pages.apps.PagesConfig',
 ]
 
--- adoption/urls.py
+++ adoption/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("", include("pages.urls")),
     path("admin/", admin.site.urls),
+    path("", include("pages.urls"))
 ]
 
 if settings.DEBUG:
